0a01188c3c9ed8693f5821c03a63e7dbe82acb94
Walter Díaz <walterwalon@gmail.com>
2022-05-20 09:27:16 -0600

    Render Gnosis --Eslint disable

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withPWA = require("next-pwa");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   pwa: {
     dest: "public",
     register: true,
